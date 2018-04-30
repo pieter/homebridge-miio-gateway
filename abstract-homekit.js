6cd91fb200c67866086a5a93a20a80d23f171f08
Pieter de Bie <pieter@frim.nl>
2018-04-30 15:17:29 +0200

    Refactor capabilities
    This moves the capability logic into a separate module

---
+++ abstract-homekit.js
@@ -0,0 +1,121 @@
+let Service, Characteristic, log;
+
+const capabilityMap = {
+	'cap:actions': addSwitch,
+	'cap:temperature': addTemperatureSensor,
+	'cap:motion': addMotionSensor,
+	'cap:illuminance': addIllumination,
+	'cap:battery-level': addBatteryLevel
+}
+
+exports.init = function(service, characteristic) {
+	Service = service;
+	Characteristic = characteristic;
+}
+
+exports.setLog = function(_log) { log = _log; }
+
+exports.decorateAccessory = function(accessory, device) {
+	log.debug("Found a device", device);
+	for (const cap in capabilityMap) {
+		if (device.matches(cap)) {
+			capabilityMap[cap](device, accessory);
+		}
+	}
+}
+
+function addIllumination(device, accessory) {
+	log.debug("Adding Light Sensor service");
+	
+  const service = accessory.findOrCreateService(Service.LightSensor, "Light Sensor");
+  const lightLevel = service.getCharacteristic(Characteristic.CurrentAmbientLightLevel);
+	
+	device.on('illuminanceChanged', (newVal) => {
+		log.debug(`${accessory.displayName} illuminance changed`, newVal);
+		lightLevel.updateValue(newVal.value);
+	});
+
+	// Set initial illuminance
+	device.illuminance().then(x =>  {
+		log.debug(`Got initial illuminance for ${accessory.displayName}:`, x.value);
+		lightLevel.updateValue(x.value);
+	});
+}
+
+function addBatteryLevel(device, accessory) {
+	log.debug("Adding Battery Level service");
+	
+  const service = accessory.findOrCreateService(Service.BatteryService, "Battery Level");
+  const batteryLevel = service.getCharacteristic(Characteristic.BatteryLevel);
+	service.getCharacteristic(Characteristic.ChargingState).updateValue(Characteristic.ChargingState.NOT_CHARGEABLE);
+
+	device.on('batteryLevelChanged', (newVal) => {
+		log.debug(`${accessory.displayName} battery level changed`, newVal);
+		batteryLevel.updateValue(newVal.value);
+	});
+
+	// Set initial illuminance
+	device.batteryLevel().then(x =>  {
+		log.debug(`Got initial battery level for ${accessory.displayName}:`, x);
+		batteryLevel.updateValue(x);
+	});
+}
+
+
+function addSwitch(device, accessory) {
+	log.debug("Adding Switch service");
+	
+  const service = accessory.findOrCreateService(Service.StatelessProgrammableSwitch, "Click");
+  const switchEvent = service.getCharacteristic(Characteristic.ProgrammableSwitchEvent);
+
+	const actionMap = {
+		'click': Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS,
+		'double_click': Characteristic.ProgrammableSwitchEvent.DOUBLE_PRESS,
+		'long_click_press': Characteristic.ProgrammableSwitchEvent.LONG_PRESS
+	}
+	
+	device.on('action', event => {
+		log.debug(`Button clicked. Action: ${event.action}`);
+		
+		if (actionMap[event.actionMap]) {
+			switchEvent.setValue(actionMap[event.action]);
+		} else {
+			log.debug(`Action ${event.action} not implemented, doing nothing`);
+		}
+		
+	});
+}
+
+function addMotionSensor(device, accessory) {
+	log.debug("Adding Motion Sensor service");
+
+  const service = accessory.findOrCreateService(Service.MotionSensor, "Motion");
+  const motionEvent = service.getCharacteristic(Characteristic.MotionDetected);
+
+	device.on('movement', event => {
+		log.debug(`Motion Detected. Event: ${event}`);
+		motionEvent.updateValue(true);
+	});
+
+	device.on('inactivity', event => {
+		log.debug(`It's over now'`);
+		motionEvent.updateValue(false);
+	});
+}
+
+function addTemperatureSensor(device, accessory) {
+	log.debug("Adding Temperature Sensor service");
+
+  const service = accessory.findOrCreateService(Service.TemperatureSensor, "Temperature");
+  const temperatureLevel = service.getCharacteristic(Characteristic.CurrentTemperature);
+
+	device.on('temperatureChanged', temp => {
+		log.debug(`New Temperature:`, temp);
+		temperatureLevel.updateValue(temp.value);
+	});
+
+	device.temperature().then(temp => {
+		log.debug(`Received initial Temperature:`, temp);
+		temperatureLevel.updateValue(temp.value);
+	});
+}
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const miio = require('miio');
 let Accessory, Service, Characteristic, UUIDGen;
 
+const abstractHomekit = require('./abstract-homekit');
+
 module.exports = function(homebridge) {
   // Accessory must be created from PlatformAccessory Constructor
   Accessory = homebridge.platformAccessory;
@@ -10,15 +12,17 @@ module.exports = function(homebridge) {
   Characteristic = homebridge.hap.Characteristic;
   UUIDGen = homebridge.hap.uuid;
 
+	abstractHomekit.init(Service, Characteristic, this.log);
+	
   // register platform in to homebridge
   homebridge.registerPlatform("homebridge-miio-gateway", "XiaomiMiioGateway", XiaomiMiioGateway, true);
 }
 
 function XiaomiMiioGateway(log, config, api) {
   log("Setting up Miio platform");
-  var platform = this;
+	abstractHomekit.setLog(log);
+	
   this.log = log;
-	console.dir(log);
   this.config = config || {};
   this.accessories = {};
   this.api = api;
@@ -64,27 +68,8 @@ XiaomiMiioGateway.prototype.addConnectedDevice = async function(device) {
 	this.log.debug("Found a device", device);
 
 	const accessory = this.findOrCreateAccessory(device);
+	abstractHomekit.decorateAccessory(accessory, device);
 		
-	if (device.matches('cap:actions')) {
-		this.addSwitch(device, accessory);
-	}
-	
-	if (device.matches('cap:temperature')) {
-		this.addTemperatureSensor(device, accessory);
-	} 
-	
-	if (device.matches('cap:motion')) {
-		this.addMotionSensor(device, accessory);
-	}
-	
-	if (device.matches('cap:illuminance')) {
-		this.addIllumination(device, accessory);
-	}
-	
-	if (device.matches('cap:battery-level')) {
-		this.addBatteryLevel(device, accessory);
-	}
-	
 	// Check if anything added a service; we have 1 service by default.
 	if (accessory.hasRealServices()) {
 		this.log.debug(`Got ${accessory.services.length} services for ${accessory.displayName}, adding it to HomeKit`);
@@ -96,102 +81,6 @@ XiaomiMiioGateway.prototype.addConnectedDevice = async function(device) {
 	} else {
 		this.log.debug(`Looks like we didn't add any services to this ${device.miioModel || device.model}, skipping it`);
 	}
-}
-
-XiaomiMiioGateway.prototype.addIllumination = function(device, accessory) {
-	this.log.debug("Adding Light Sensor service");
-	
-  const service = accessory.findOrCreateService(Service.LightSensor, "Light Sensor");
-  const lightLevel = service.getCharacteristic(Characteristic.CurrentAmbientLightLevel);
-	
-	device.on('illuminanceChanged', (newVal) => {
-		this.log.debug(`${accessory.displayName} illuminance changed`, newVal);
-		lightLevel.updateValue(newVal.value);
-	});
-
-	// Set initial illuminance
-	device.illuminance().then(x =>  {
-		this.log.debug(`Got initial illuminance for ${accessory.displayName}:`, x.value);
-		lightLevel.updateValue(x.value);
-	});
-}
-
-XiaomiMiioGateway.prototype.addBatteryLevel = function(device, accessory) {
-	this.log.debug("Adding Battery Level service");
-	
-  const service = accessory.findOrCreateService(Service.BatteryService, "Battery Level");
-  const batteryLevel = service.getCharacteristic(Characteristic.BatteryLevel);
-	service.getCharacteristic(Characteristic.ChargingState).updateValue(Characteristic.ChargingState.NOT_CHARGEABLE);
-
-	device.on('batteryLevelChanged', (newVal) => {
-		this.log.debug(`${accessory.displayName} battery level changed`, newVal);
-		batteryLevel.updateValue(newVal.value);
-	});
-
-	// Set initial illuminance
-	device.batteryLevel().then(x =>  {
-		this.log.debug(`Got initial battery level for ${accessory.displayName}:`, x);
-		batteryLevel.updateValue(x);
-	});
-}
-
-
-XiaomiMiioGateway.prototype.addSwitch = function(device, accessory) {
-	this.log.debug("Adding Switch service");
-	
-  const service = accessory.findOrCreateService(Service.StatelessProgrammableSwitch, "Click");
-  const switchEvent = service.getCharacteristic(Characteristic.ProgrammableSwitchEvent);
-
-	const actionMap = {
-		'click': Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS,
-		'double_click': Characteristic.ProgrammableSwitchEvent.DOUBLE_PRESS,
-		'long_click_press': Characteristic.ProgrammableSwitchEvent.LONG_PRESS
-	}
-	
-	device.on('action', event => {
-		this.log.debug(`Button clicked. Action: ${event.action}`);
-		
-		if (actionMap[event.actionMap]) {
-			switchEvent.setValue(actionMap[event.action]);
-		} else {
-			this.log.debug(`Action ${event.action} not implemented, doing nothing`);
-		}
-		
-	});
-}
-
-XiaomiMiioGateway.prototype.addMotionSensor = function(device, accessory) {
-	this.log.debug("Adding Motion Sensor service");
-
-  const service = accessory.findOrCreateService(Service.MotionSensor, "Motion");
-  const motionEvent = service.getCharacteristic(Characteristic.MotionDetected);
-
-	device.on('movement', event => {
-		this.log.debug(`Motion Detected. Event: ${event}`);
-		motionEvent.updateValue(true);
-	});
-
-	device.on('inactivity', event => {
-		this.log.debug(`It's over now'`);
-		motionEvent.updateValue(false);
-	});
-}
-
-XiaomiMiioGateway.prototype.addTemperatureSensor = function(device, accessory) {
-	this.log.debug("Adding Temperature Sensor service");
-
-  const service = accessory.findOrCreateService(Service.TemperatureSensor, "Temperature");
-  const temperatureLevel = service.getCharacteristic(Characteristic.CurrentTemperature);
-
-	device.on('temperatureChanged', temp => {
-		this.log.debug(`New Temperature:`, temp);
-		temperatureLevel.updateValue(temp.value);
-	});
-
-	device.temperature().then(temp => {
-		this.log.debug(`Received initial Temperature:`, temp);
-		temperatureLevel.updateValue(temp.value);
-	});
 }
 
 XiaomiMiioGateway.prototype.findOrCreateAccessory = function(device, name, onInit) {
